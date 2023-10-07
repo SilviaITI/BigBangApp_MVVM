0d140d5fa211248f8c92c723a3318d2019f30d2c
silviaITI <silvia.casanovamartinez@gmail.com>
2023-10-07 13:41:04 +0200

    built structure

---
+++ BigBangApp_MVVM/BigBangApp_MVVM/Model/Episode.swift
@@ -0,0 +1,21 @@
+//
+//  Episode.swift
+//  BigBangApp_MVVM
+//
+//  Created by Silvia Casanova Martinez on 7/10/23.
+//
+
+import Foundation
+
+struct Episode {
+let season: Int
+let number: Int
+let summary: String
+let runtime: Int
+let airdate: Date
+let image: String
+let name: String
+    
+}
+
+
